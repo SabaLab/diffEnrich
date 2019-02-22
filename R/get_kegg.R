18e18e82e6bd48d481401208c948497f0ae6d533
Harry Smith <harry.smith@ucdenver.edu>
2019-02-22 16:15:55 -0700

    Built api paths

--- R/get_kegg.R
+++ R/get_kegg.R
@@ -7,13 +7,27 @@
 #' @export
 #'
 #' @examples
-get_kegg <- function(){
+get_kegg <- function(species){
+  # Define user's base file path
+  base_path <- here::here()
   # Define base api path and define list of operations/arguments
   api_base <- "http://rest.kegg.jp/"
-  op <- as.list("info"="info", "list"="list", "find"="find", "get"="get",
+  op <- list("info"="info", "list"="list", "find"="find", "get"="get",
                 "conv"="conv", "link"="link", "ddi"="ddi")
-  db <- as.list("pathway"="pathway")
-  org <- as.list("human"="hsa", "mouse"="mmu", "rat"="rno")
+  db <- list("pathway"="pathway")
+  org <- list("human"="hsa", "mouse"="mmu", "rat"="rno")
+  # Build api paths for
+  # 1) ncbi to kegg
+  ncbi_to_kegg_path <- paste0(api_base, op[["conv"]], "/",
+                              org[[species]], "/", "ncbi")
+  # 2) kegg to pathway
+  kegg_to_pathway_path <- paste0(api_base, op[["link"]], "/", db[["pathway"]],
+                                 "/", org[[species]])
+  # 3) pathway to species
+  pathway_to_species_path <- paste0(api_base, op[["list"]], "/",
+                                    db[["pathway"]],
+                                 "/", org[[species]])
+
 
 
 }
