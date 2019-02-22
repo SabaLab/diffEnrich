307967c384630709e7a9d6d0ebc88fe345004047
Harry Smith <harry.smith@ucdenver.edu>
2019-02-22 15:42:59 -0700

    Initiated get_kegg function

---
+++ R/get_kegg.R
@@ -0,0 +1,12 @@
+#' @title get_kegg
+#' @description This function connects to KEGG API, downloads, and cleans
+#' ncbi gene ID data, KEGG pathway descriptions, and species specific data.
+#' Currently, this function supports Human, Mouse, and Rat.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_kegg <- function(){
+
+}
