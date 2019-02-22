d4201197f8d8e0ece9f8764df1980ace59d5643c
Harry Smith <harry.smith@ucdenver.edu>
2019-02-22 15:55:40 -0700

    defined api options/arguments

--- R/get_kegg.R
+++ R/get_kegg.R
@@ -8,5 +8,12 @@
 #'
 #' @examples
 get_kegg <- function(){
+  # Define base api path and define list of operations/arguments
+  api_base <- "http://rest.kegg.jp/"
+  op <- as.list("info"="info", "list"="list", "find"="find", "get"="get",
+                "conv"="conv", "link"="link", "ddi"="ddi")
+  db <- as.list("pathway"="pathway")
+  org <- as.list("human"="hsa", "mouse"="mmu", "rat"="rno")
+
 
 }
