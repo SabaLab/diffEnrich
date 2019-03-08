6a6d81fdac2749c3d78d337811e303300ee64881
Harry Smith <harry.smith@ucdenver.edu>
2019-03-08 13:20:30 -0700

    Had to make structure changes after adding kegg release

--- R/get_kegg.R
+++ R/get_kegg.R
@@ -28,11 +28,6 @@ get_kegg <- function(species){
   base_path <- here::here()
   flist <- list.files(base_path)
   if(missing(species)){stop("Must choose one of the 3 species options: human, mouse, rat")}
-  if (sum(flist %in% c(paste("ncbi_to_kegg",Sys.Date(), kegg_release, ".txt",sep=""),
-                       paste("kegg_to_pathway",Sys.Date(), kegg_release, ".txt",sep=""),
-                       paste("pathway_to_species",Sys.Date(), kegg_release, ".txt",sep="")))>0){
-    stop("These files already exist in your working directory.")
-  }
   else {
     # Define base api path and define list of operations/arguments
     api_base <- "http://rest.kegg.jp/"
@@ -55,51 +50,60 @@ get_kegg <- function(species){
     pathway_to_kegg_release <- paste0(api_base, op[["info"]], "/",
                                       "kegg")
     ## api pull
-    ncbi_to_kegg <- utils::read.table(file = ncbi_to_kegg_path,
-                               fill = TRUE,
-                               sep = "\t",
-                               quote = "")
-    kegg_to_pathway <- utils::read.table(file = kegg_to_pathway_path,
-                                  fill = TRUE,
-                                  sep = "\t",
-                                  quote = "")
-    pathway_to_species <- utils::read.table(file = pathway_to_species_path,
-                                     fill = TRUE,
-                                     sep = "\t",
-                                     quote = "")
     kegg_release <- utils::read.table(file = pathway_to_kegg_release,
-                                            fill = TRUE,
-                                            sep = "\t",
-                                            quote = "")[2, 1]
+                                      fill = TRUE,
+                                      sep = "\t",
+                                      quote = "")[2, 1]
     kegg_release <- stringr::str_extract(kegg_release, ".{0,0}Release.{0,30}")
     kegg_release <- gsub(",", "", kegg_release, fixed = T)
     kegg_release <- gsub(" ", "_", kegg_release, fixed = T)
-    message("3 data sets will be written as tab delimited text files")
-    message("File location: ", here::here())
-    message("Kegg Release: ", kegg_release)
-    ## Since the kegg api will pull the most updated verions
-    # write out tables for reproduciblity.
-    utils::write.table(ncbi_to_kegg,
-                file=paste(base_path,"/ncbi_to_kegg",Sys.Date(), kegg_release, ".txt",sep=""),
-                sep="\t",
-                row.names=FALSE,
-                col.names=FALSE,
-                quote=FALSE)
-    utils::write.table(kegg_to_pathway,
-                file=paste(base_path,"/kegg_to_pathway",Sys.Date(), kegg_release, ".txt",sep=""),
-                sep="\t",
-                row.names=FALSE,
-                col.names=FALSE,
-                quote=FALSE)
-    utils::write.table(pathway_to_species,
-                file=paste(base_path,"/pathway_to_species",Sys.Date(), kegg_release, ".txt",sep=""),
-                sep="\t",
-                row.names=FALSE,
-                col.names=FALSE,
-                quote=FALSE)
-    kegg_out <- list("ncbi_to_kegg" = ncbi_to_kegg,
-                     "kegg_to_pathway" = kegg_to_pathway,
-                     "pathway_to_species", pathway_to_species)
-    return(kegg_out)
+    kegg_release <- gsub("/", "_", kegg_release, fixed = T)
+    if (sum(flist %in% c(paste("ncbi_to_kegg",Sys.Date(), kegg_release, ".txt",sep=""),
+                         paste("kegg_to_pathway",Sys.Date(), kegg_release, ".txt",sep=""),
+                         paste("pathway_to_species",Sys.Date(), kegg_release, ".txt",sep="")))>0){
+      stop("These files already exist in your working directory.")
+    }
+    else {
+      ncbi_to_kegg <- utils::read.table(file = ncbi_to_kegg_path,
+                                        fill = TRUE,
+                                        sep = "\t",
+                                        quote = "")
+      kegg_to_pathway <- utils::read.table(file = kegg_to_pathway_path,
+                                           fill = TRUE,
+                                           sep = "\t",
+                                           quote = "")
+      pathway_to_species <- utils::read.table(file = pathway_to_species_path,
+                                              fill = TRUE,
+                                              sep = "\t",
+                                              quote = "")
+
+      message("3 data sets will be written as tab delimited text files")
+      message("File location: ", here::here())
+      message("Kegg Release: ", kegg_release)
+      ## Since the kegg api will pull the most updated verions
+      # write out tables for reproduciblity.
+      utils::write.table(ncbi_to_kegg,
+                         file=paste(base_path,"/ncbi_to_kegg",Sys.Date(), kegg_release, ".txt",sep=""),
+                         sep="\t",
+                         row.names=FALSE,
+                         col.names=FALSE,
+                         quote=FALSE)
+      utils::write.table(kegg_to_pathway,
+                         file=paste(base_path,"/kegg_to_pathway",Sys.Date(), kegg_release, ".txt",sep=""),
+                         sep="\t",
+                         row.names=FALSE,
+                         col.names=FALSE,
+                         quote=FALSE)
+      utils::write.table(pathway_to_species,
+                         file=paste(base_path,"/pathway_to_species",Sys.Date(), kegg_release, ".txt",sep=""),
+                         sep="\t",
+                         row.names=FALSE,
+                         col.names=FALSE,
+                         quote=FALSE)
+      kegg_out <- list("ncbi_to_kegg" = ncbi_to_kegg,
+                       "kegg_to_pathway" = kegg_to_pathway,
+                       "pathway_to_species", pathway_to_species)
+      return(kegg_out)
+    }
   }
 }
