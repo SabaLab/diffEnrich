87303acec3dcb19c5664478222e2377f1f605202
Harry Smith <harry.smith@ucdenver.edu>
2019-03-27 13:26:59 -0600

    added argument checks

--- R/pathEnrich.R
+++ R/pathEnrich.R
@@ -4,6 +4,9 @@
 #' @export
 #'
 #' @examples
-pathEnrich <- function(){
+pathEnrich <- function(gk_obj, gene_list){
+  ## argument check
+  if(missing(gk_obj)){stop("Argument missing: gk_obj")}
+  if(missing(gene_list)){stop("Argument missing: gene_list. Please provide list of ncbi geneIDs")}
 
 }
