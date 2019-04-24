2bfffa9388c958764eef003a623e87e38151772c
Harry Smith <harry.smith@ucdenver.edu>
2019-04-24 10:00:22 -0600

    Added sample_data documentation

---
+++ R/sample_data.R
@@ -0,0 +1,12 @@
+#' @title geneLists
+#'
+#' @description This is a \code{list} object that contains the list background
+#' genes and significant genes used in pathway enrichment. This object is
+#' mostly meant fo running examples and vignettes.
+#'
+#' @format A \code{list} with two names items which are:
+#' \describe{
+#' \item{background}{List of ENTREZ gene IDs that will considered background }
+#' \item{sigGenes}{ List of ENTREZ gene IDs that were significant}
+#' }
+"geneLists"
