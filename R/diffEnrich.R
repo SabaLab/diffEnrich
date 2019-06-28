cba7861e15179e86f45c2bbc65949729e06f2e60
Harry Smith <harry.smith@ucdenver.edu>
2019-06-28 15:35:18 -0600

    Turned combineEnrich into helper and added roxygen skeleton for diffEnrich

--- R/diffEnrich.R
+++ R/diffEnrich.R
@@ -1,12 +1,57 @@
-#' combineEnrich
-#' @description This function takes the objects generated from \code{\link{pathEnrich}}.
+#' diffEnrich
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+diffEnrich <- function(x){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' .combineEnrich
+#' @description This is a helper function for \code{diffEnrich}. This function takes the objects generated from \code{\link{pathEnrich}}.
 #' If performing a dfferential enrichment analysis, the user will have 2 objects. There
 #' will be one for the genes of interest and one for the background (see example for \code{\link{pathEnrich}}).
 #' This function then merges the two dataframes using the following columns that should be present
 #' in both objects (\code{by = c("V1", "V2", "KEGG_cnt", "numTested")}). This merged dataframe
-#' will be used as the input for the differential enrichment function. In addition to this
-#' merged dataframe, this function will produce a table summarizing the number of significantly
-#' enriched genes in each original object, based on a user defined fdr threshold.
+#' will be used as the input for the differential enrichment function.
 #'
 #' @param sig_pe data.frame. Dataframe of enrichment results for genes of interest
 #' generated from \code{\link{pathEnrich}}. See example for \code{\link{pathEnrich}}.
@@ -17,16 +62,8 @@
 #' \code{sig_pe} and \code{bkg_pe}, using the default joining implemented in the base
 #' \code{\link{merge}} function.
 #'
-#' @export
 #'
-#' @examples
-#' ## Generate individual enrichment results
-#' sig_pe <- pathEnrich(gk_obj = kegg, gene_list = geneLists$sigGenes)
-#' bkg_pe <- pathEnrich(gk_obj = kegg, gene_list = geneLists$background)
-#' ## Combine
-#' combined_enrich <- combineEnrich(sig_pe, bkg_pe)
-#'
-combineEnrich <- function(sig_pe, bkg_pe){
+.combineEnrich <- function(sig_pe, bkg_pe){
   ## argument check
   if(missing(sig_pe)){stop("Argument missing: sig_pe")}
   if(missing(bkg_pe)){stop("Argument missing: bkg_pe")}
