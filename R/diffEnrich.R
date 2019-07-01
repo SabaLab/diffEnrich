396d6389334e6ffc8afe0dd53a2d3ab26b04f41a
Harry Smith <harry.smith@ucdenver.edu>
2019-07-01 15:33:54 -0600

    Added colname descriptions to diffEnrich documentation

--- R/diffEnrich.R
+++ R/diffEnrich.R
@@ -16,9 +16,28 @@
 #' use for multiple testing correction. Available methods are thos provided by
 #' \code{\link{p.adjust}}, and the default is "BH", or False Discovery Rate (FDR).
 #'
-#' @return data.frame. Dataframe generated from merging pathEnrich dataframes with the following added columns:
-#'                     Estimate: Estimated odds ration calculated from Fisher's Exact test
-#'                     P_value: Unadjusted p_value from Fisher's Exact test
+#' @return res: An object of class data.frame that summarizes the results
+#' of the differential enrichment and contains the following variables:
+#'
+#' \describe{
+#'   \item{KEGG_PATHWAY_ID}{KEGG Pathway Identifier}
+#'   \item{KEGG_PATHWAY_description}{Description of KEGG Pathway (provided by KEGG)}
+#'   \item{KEGG_PATHWAY_cnt}{Number of Genes in KEGG Pathway}
+#'   \item{KEGG_DATABASE_cnt}{Number of Genes in KEGG Database}
+#'   \item{KEGG_PATHWAY_in_list1}{Number of Genes from gene list 1 in KEGG Pathway}
+#'   \item{KEGG_DATABASE_in_list1}{Number of Genes from gene list 1 in KEGG Database}
+#'   \item{expected_list1}{Expected number of genes from list 1 to be in KEGG pathway by chance (i.e., not enriched)}
+#'   \item{enrich_p_list1}{P-value for enrichment of list 1 genes related to KEGG pathway}
+#'   \item{fdr_list1}{False Discovery Rate (Benjamini and Hochberg) of enrich_p_list1 to account for multiple testing across KEGG pathways}
+#'   \item{KEGG_PATHWAY_in_list2}{Number of Genes from gene list 2 in KEGG Pathway}
+#'   \item{KEGG_DATABASE_in_list2}{Number of Genes from gene list 2 in KEGG Database}
+#'   \item{expected_list2}{Expected number of genes from list 2 to be in KEGG pathway by chance (i.e., not enriched)}
+#'   \item{enrich_p_list2}{P-value for enrichment of list 2 genes related to KEGG pathway}
+#'   \item{fdr_list2}{False Discovery Rate (Benjamini and Hochberg) of enrich_p_list2 to account for multiple testing across KEGG pathways}
+#'   \item{odd_ratio}{Odds of a gene from list 2 being from this KEGG pathway / Odds of a gene from list 1 being from this KEGG pathway}
+#'   \item{diff_enrich_p}{P-value for differential enrichment of this KEGG pathway between list 1 and list 2}
+#'   \item{diff_enrich_adjusted}{Multiple testing adjustment of diff_enrich_p (default = False Discovery Rate (Benjamini and Hochberg))}
+#' }
 #'
 #' @export
 #' @importFrom  stats fisher.test
@@ -44,11 +63,16 @@ diffEnrich <- function(list1_pe, list2_pe, method = 'BH'){
     return(out.de)
   }
   ## perform differential enrichment
-  res <- cbind(ce, do.call('rbind', apply(ce[, c("KEGG_in_list_list2", "KEGG_in_list_list1", "numSig_list2", "numSig_list1")], 1,
+  res <- cbind(ce, do.call('rbind', apply(ce[, c("KEGG_PATHWAY_in_list2", "KEGG_PATHWAY_in_list1",
+                                                 "KEGG_DATABASE_in_list2", "KEGG_DATABASE_in_list1")], 1,
                function(a){ de(a[1], a[2], a[3], a[4])})))
   res$adjusted_p <- stats::p.adjust(res$pv, method = method)
+  colnames(res) <- c("KEGG_PATHWAY_ID", "KEGG_PATHWAY_description", "KEGG_PATHWAY_cnt", "KEGG_DATABASE_cnt",
+                     "KEGG_PATHWAY_in_list1", "KEGG_DATABASE_in_list1", "expected_list1", "enrich_p_list1",
+                     "fdr_list1", "KEGG_PATHWAY_in_list2", "KEGG_DATABASE_in_list2", "expected_list2",
+                     "enrich_p_list2", "fdr_list2", "odd_ratio", "diff_enrich_p", "diff_enrich_adjusted")
   ## update rownames
-  rownames(res) <- res$KEGG_ID
+  rownames(res) <- res$KEGG_PATHWAY_ID
   return(res)
 }
 
@@ -57,14 +81,14 @@ diffEnrich <- function(list1_pe, list2_pe, method = 'BH'){
 #' .combineEnrich
 #' @description This is a helper function for \code{diffEnrich}. This function takes the objects generated from \code{\link{pathEnrich}}.
 #' If performing a dfferential enrichment analysis, the user will have 2 objects. There
-#' will be one for the genes of interest and one for the background (see example for \code{\link{pathEnrich}}).
-#' This function then merges the two dataframes using the following columns that should be present
-#' in both objects (\code{by = c("V1", "V2", "KEGG_cnt", "numTested")}). This merged dataframe
+#' will be one for list1 and one for list2(see example for \code{\link{pathEnrich}}).
+#' This function then merges the two data frames using the following columns that should be present
+#' in both objects (\code{by = c("KEGG_PATHWAY_ID", "KEGG_PATHWAY_description", "KEGG_PATHWAY_cnt", "KEGG_DATABASE_cnt")}). This merged data frame
 #' will be used as the input for the differential enrichment function.
 #'
-#' @param list1_pe data.frame. Dataframe of enrichment results for genes of interest
+#' @param list1_pe data.frame. Data frame of enrichment results for list1
 #' generated from \code{\link{pathEnrich}}. See example for \code{\link{pathEnrich}}.
-#' @param  list2_pe data.frame. Dataframe of enrichment results for background genes
+#' @param  list2_pe data.frame. Data frame of enrichment results for list2
 #' generated from \code{\link{pathEnrich}}. See example for \code{\link{pathEnrich}}.
 #'
 #' @return combined_enrich: An object of class data.frame that is the result of merging
@@ -78,9 +102,13 @@ diffEnrich <- function(list1_pe, list2_pe, method = 'BH'){
   if(missing(list2_pe)){stop("Argument missing: list2_pe")}
 
   ## Merge results from first enrichment
-  combined_enrich <- merge(list1_pe, list2_pe, by = c("KEGG_ID", "KEGG_description", "KEGG_cnt", "numTested"))
+  combined_enrich <- merge(list1_pe, list2_pe, by = c("KEGG_PATHWAY_ID", "KEGG_PATHWAY_description", "KEGG_PATHWAY_cnt", "KEGG_DATABASE_cnt"))
   colnames(combined_enrich) <- gsub(".x", "_list1", colnames(combined_enrich), fixed = TRUE)
   colnames(combined_enrich) <- gsub(".y", "_list2", colnames(combined_enrich), fixed = TRUE)
+  colnames(combined_enrich) <- c("KEGG_PATHWAY_ID", "KEGG_PATHWAY_description", "KEGG_PATHWAY_cnt", "KEGG_DATABASE_cnt",
+                                 "KEGG_PATHWAY_in_list1", "KEGG_DATABASE_in_list1", "expected_list1", "enrich_p_list1",
+                                 "fdr_list1", "KEGG_PATHWAY_in_list2", "KEGG_DATABASE_in_list2", "expected_list2",
+                                 "enrich_p_list2", "fdr_list2")
 
   out <- combined_enrich
   return(out)
