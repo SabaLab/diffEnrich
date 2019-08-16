1d0eae5080340c1df92805c3b9cc21f933a2d169
Harry Smith <harry.smith@ucdenver.edu>
2019-08-16 15:06:53 -0600

    updated argument checks

--- R/plotFoldEnrichment.R
+++ R/plotFoldEnrichment.R
@@ -3,7 +3,7 @@
 #' to generate a bar plot describing the fold enrichment of a set of given KEGG
 #' pathways stratified by their enrichment in list 1 or list 2. Users can plot
 #' all pathways based on the adjusted p-value threshold used in \code{\link{diffEnrich}}
-#' or the top N pathways sorted by the adjusted p-value threshold used in \code{\link{diffEnrich}}.
+#' and the top N pathways sorted by the adjusted p-value threshold used in \code{\link{diffEnrich}}.
 #' \code{\link{plotFoldEnrich}} returns a ggplot2 object so users can add additional
 #' customizations.
 #' @param de_res
@@ -19,8 +19,9 @@
 plotFoldEnrichment <- function(de_res, pval, N){
   ## Check arguments
   if(missing(de_res)){stop("Argument missing: de_res")}
-  if(missing(pval)){stop("Argument missing: pval - if you'd like to plot based on pvalue, please provide a threshold and make sure N = NULL")}
-  if(missing(N)){stop("Argument missing: N - if you'd like to plot based on top pathways, please provide a threshold and make sure N = N")}
+  if(missing(pval)){stop("Argument missing: pval - please provide a threshold")}
+  if(missing(N)){stop("Argument missing: N - if you'd like to plot top pathways, please provide a threshold and make sure N > 0")}
+  if(N < 1){stop("Number of top genes (N) must be > 0")}
 
   ## Strip extra columns from de_res and filter based on pval. Then sort by pval.
   df <- de_res %>%
