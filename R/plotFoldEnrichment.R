5faccaad6fdd3f64bf1603b01f63e38ca369184f
Harry Smith <harry.smith@ucdenver.edu>
2019-08-22 10:42:59 -0600

    returned p and tested

--- R/plotFoldEnrichment.R
+++ R/plotFoldEnrichment.R
@@ -76,16 +76,17 @@ plotFoldEnrichment <- function(de_res, pval, N){
               aes(x = KEGG_PATHWAY_description, y = (max(bardat$value) + 0.3), label = round(value, 4))) +
     labs(alpha = "List specific p-value")
   ## TEST 2
-  p2 <- ggplot(bardat, aes(x=reorder(KEGG_PATHWAY_description, -pvals), y=value)) +
-    geom_bar(stat="identity", aes(group=variable, fill=pvals), position="dodge") +
-    ylim(0, max(bardat$value) + 0.6) + xlab("") +
-    coord_flip() +
-    scale_fill_brewer(palette = "Set1",
-                      name="",
-                      breaks=c("fold_enrichment_list1", "fold_enrichment_list2"),
-                      labels=c("Fold Enrichment in \nlist 1\n", "Fold enrichment in \nlist 2\n")) +
-    scale_fill_continuous(trans = 'log10') +
-    geom_text(data=subset(df.ss, variable %in% c("diff_enrich_adjusted")),
-              aes(x = KEGG_PATHWAY_description, y = (max(bardat$value) + 0.3), label = round(value, 4))) +
-    labs(alpha = "List specific p-value")
+  # p2 <- ggplot(bardat, aes(x=reorder(KEGG_PATHWAY_description, -pvals), y=value)) +
+  #   geom_bar(stat="identity", aes(group=variable, fill=pvals), position="dodge") +
+  #   ylim(0, max(bardat$value) + 0.6) + xlab("") +
+  #   coord_flip() +
+  #   scale_fill_brewer(palette = "Set1",
+  #                     name="",
+  #                     breaks=c("fold_enrichment_list1", "fold_enrichment_list2"),
+  #                     labels=c("Fold Enrichment in \nlist 1\n", "Fold enrichment in \nlist 2\n")) +
+  #   scale_fill_continuous(trans = 'log10') +
+  #   geom_text(data=subset(df.ss, variable %in% c("diff_enrich_adjusted")),
+  #             aes(x = KEGG_PATHWAY_description, y = (max(bardat$value) + 0.3), label = round(value, 4))) +
+  #   labs(alpha = "List specific p-value")
+  return(p)
 }
